cae9d605750606d4c32e63f58755842442fc014b
Kenta Tanaka <42142120+kenken-neko@users.noreply.github.com>
2021-05-16 19:50:05 +0900

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+import tensorflow_datasets as tfds
+from tensorflow.keras.models import load_model
+
+from preprocess import ImagePreprocess
+
+
+def main(
+    dataset_name, 
+    model_type,
+    model_dir,
+    batch_size,
+):
+    # Load test dataset
+    test_dataset, info = tfds.load(
+        name=dataset_name, 
+        split="test", 
+        with_info=True,
+    )
+    _, _, channel_size = info.features["image"].shape
+    num_classes = info.features["label"].num_classes
+
+    # Image preprocess
+    preprocess = ImagePreprocess(
+        channel_size=channel_size,
+        num_classes=num_classes,
+    )
+    if model_type == "SimpleCNN":
+        test_dataset = test_dataset.map(preprocess.base_preprocess)
+        test_dataset = test_dataset.batch(batch_size)
+    elif model_type == "VGG16":
+        test_dataset = test_dataset.map(preprocess.vgg_preprocess)
+        test_dataset = test_dataset.batch(batch_size)
+    elif model_type == "Xception":
+        test_dataset = test_dataset.map(preprocess.xception_preprocess)
+        test_dataset = test_dataset.batch(batch_size)
+    else:
+        raise ValueError(f"The model: {model_type} does not exist.")
+
+    # Load model
+    model_file_name = model_type + ".h5"
+    model_path = os.path.join(model_dir, model_file_name)
+    model = load_model(model_path)
+
+    # Evaluation
+    test_loss, test_acc = model.evaluate(test_dataset)
+    print(f"test_loss: {test_loss}, test_acc: {test_acc}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parameters for evaluate task")
+    parser.add_argument("--dataset_name", type=str, default="mnist")  # Ex.: mnist, fashion_mnist, cifar10
+    parser.add_argument("--model_type", type=str, default="SimpleCNN")
+    parser.add_argument("--model_dir", default="models")
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
+    main(
+        dataset_name=args.dataset_name,
+        model_type=args.model_type,
+        model_dir=args.model_dir,
+        batch_size=args.batch_size,
+    )
