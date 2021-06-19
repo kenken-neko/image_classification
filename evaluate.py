1d18a8a92a58a7bf823cfdf4f185dcff8cd9b3b0
Kenta Tanaka <42142120+kenken-neko@users.noreply.github.com>
2021-06-19 22:37:14 +0900

    fix batch size

--- evaluate.py
+++ evaluate.py
@@ -10,7 +10,6 @@ def main(
     dataset_name, 
     model_type,
     model_dir,
-    batch_size,
 ):
     # Load test dataset
     test_dataset, info = tfds.load(
@@ -30,13 +29,13 @@ def main(
     )
     if model_type == "SimpleCNN":
         test_dataset = test_dataset.map(img_prep.base_preprocess)
-        test_dataset = test_dataset.batch(batch_size)
+        test_dataset = test_dataset.batch(1)
     elif model_type == "VGG16":
         test_dataset = test_dataset.map(img_prep.vgg_preprocess)
-        test_dataset = test_dataset.batch(batch_size)
+        test_dataset = test_dataset.batch(1)
     elif model_type == "Xception":
         test_dataset = test_dataset.map(img_prep.xception_preprocess)
-        test_dataset = test_dataset.batch(batch_size)
+        test_dataset = test_dataset.batch(1)
     else:
         raise ValueError(f"The model: {model_type} does not exist.")
 
@@ -55,11 +54,9 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_name", type=str, default="mnist")  # Ex.: mnist, fashion_mnist, cifar10
     parser.add_argument("--model_type", type=str, default="SimpleCNN")
     parser.add_argument("--model_dir", default="models")
-    parser.add_argument("--batch_size", type=int, default=32)
     args = parser.parse_args()
     main(
         dataset_name=args.dataset_name,
         model_type=args.model_type,
         model_dir=args.model_dir,
-        batch_size=args.batch_size,
     )
